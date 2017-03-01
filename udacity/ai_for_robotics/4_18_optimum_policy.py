6e0f72642ba36a37110439c33523d1cc9e524da3
Frank Carey <frank@devinci.io>
2017-03-01 16:07:12 -0500

    Reset the test to redo it

--- udacity/ai_for_robotics/4_18_optimum_policy.py
+++ udacity/ai_for_robotics/4_18_optimum_policy.py
@@ -1,15 +1,16 @@
 # ----------
 # User Instructions:
 #
-# Create a function optimum_policy() that returns
+# Write a function optimum_policy that returns
 # a grid which shows the optimum policy for robot
 # motion. This means there should be an optimum
-# direction associated with each navigable cell.
+# direction associated with each navigable cell from
+# which the goal can be reached.
 #
-# un-navigable cells must contain an empty string
-# WITH a space, as shown in the previous video.
-# Don't forget to mark the goal with a '*'
-
+# Unnavigable cells as well as cells from which
+# the goal cannot be reached should have a string
+# containing a single space (' '), as shown in the
+# previous video. The goal cell should have '*'.
 # ----------
 
 grid = [[0, 1, 0, 0, 0, 0],
@@ -17,27 +18,22 @@ grid = [[0, 1, 0, 0, 0, 0],
         [0, 1, 0, 0, 0, 0],
         [0, 1, 0, 0, 0, 0],
         [0, 0, 0, 0, 1, 0]]
-
 init = [0, 0]
-goal = [len(grid) - 1, len(grid[0]) - 1]
+goal = [len(grid)-1, len(grid[0])-1]
+cost = 1 # the cost associated with moving from a cell to an adjacent one
 
-delta = [[-1, 0],  # go up
-         [0, -1],  # go left
-         [1, 0],  # go down
-         [0, 1]]  # go right
+delta = [[-1, 0 ], # go up
+         [ 0, -1], # go left
+         [ 1, 0 ], # go down
+         [ 0, 1 ]] # go right
 
 delta_name = ['^', '<', 'v', '>']
 
-cost_step = 1  # the cost associated with moving from a cell to an adjacent one.
-
-
-# ----------------------------------------
-# modify code below
-# ----------------------------------------
-
-def optimum_policy():
+def optimum_policy(grid,goal,cost):
+    # ----------------------------------------
+    # modify code below
+    # ----------------------------------------
     value = [[99 for row in range(len(grid[0]))] for col in range(len(grid))]
-    policy = [[' ' for row in range(len(grid[0]))] for col in range(len(grid))]
     change = True
 
     while change:
@@ -48,7 +44,7 @@ def optimum_policy():
                 if goal[0] == x and goal[1] == y:
                     if value[x][y] > 0:
                         value[x][y] = 0
-                        policy[x][y] = '*'
+
                         change = True
 
                 elif grid[x][y] == 0:
@@ -57,17 +53,10 @@ def optimum_policy():
                         y2 = y + delta[a][1]
 
                         if x2 >= 0 and x2 < len(grid) and y2 >= 0 and y2 < len(grid[0]) and grid[x2][y2] == 0:
-                            v2 = value[x2][y2] + cost_step
+                            v2 = value[x2][y2] + cost
 
                             if v2 < value[x][y]:
                                 change = True
                                 value[x][y] = v2
-                                policy[x][y] = delta_name[a]
 
-    return policy  # Make sure your function returns the expected grid.
-
-
-p = optimum_policy()
-for i in range(len(p)):
-    print p[i]
-
+    return policy
